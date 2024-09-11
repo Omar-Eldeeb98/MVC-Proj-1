4710f962d21539060fc652221a3ece62c9aa045f
Omar-Eldeeb98 <oeldeeb111@gmail.com>
2024-09-11 21:11:46 +0300

    Add project files.

---
+++ MVC-Proj-1/Controllers/MoviesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Proj_1.Models;
+
+namespace MVC_Proj_1.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // actions
+
+        //public string getMovies(string id)
+        //{
+        //    return $"id = {id}";
+        //}
+
+        //[ActionName("testzero")]
+        public IActionResult getMovies(int? id, string name, Movie movie)
+        {
+            return Content($"id: {id}", "text/html");
+        }
+
+        public IActionResult test1(int? id)
+        {
+            if (id is null)
+            {
+                return Redirect("www.google.com");
+            }
+            else
+            {
+                return RedirectToAction("getMovies", "Movies", new { id = id });
+            }
+        }
+    }
+}
